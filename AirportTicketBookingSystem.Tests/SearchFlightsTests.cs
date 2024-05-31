3fd767030102d79f2fa148ab16e657270fb9c7a5
Maria Romero <mmrbalcazar@gmail.com>
2024-05-31 14:00:00 -0300

    SearchFlightTests refactored

--- AirportTicketBookingSystem.Tests/SearchFlightsTests.cs
+++ AirportTicketBookingSystem.Tests/SearchFlightsTests.cs
@@ -10,26 +10,28 @@ namespace AirportTicketBookingSystem.Tests
 {
     public class SearchFlightsTests
     {
+        private readonly Fixture _fixture;
+
         public SearchFlightsTests()
         {
-
+            _fixture = new Fixture();
         }
 
         [Theory]
         [InlineAutoData(100, 200)]
         [InlineAutoData(150, 250)]
         [InlineAutoData(50, 150)]
-        public void ShouldGetAFlightIfIsIncludedInThePriceRange(double lowerSearchLimit, double upperSearchLimit)
+        public void ShouldReturnFlightWithinPriceRange(double lowerSearchLimit, double upperSearchLimit)
         {
             //Arrange
-            var fixture = new Fixture();
             var random = new Random();
+            double price = random.NextDouble() * (upperSearchLimit - lowerSearchLimit) + lowerSearchLimit;
 
-            var flightAvailabilityWithinRange = fixture.Build<FlightAvailability>()
-                .With(x => x.Price, random.NextDouble() * (upperSearchLimit - lowerSearchLimit) + lowerSearchLimit)
+            var flightAvailabilityWithinRange = _fixture.Build<FlightAvailability>()
+                .With(x => x.Price, price)
                 .Create();
 
-            var flight = fixture.Build<Flight>()
+            var flight = _fixture.Build<Flight>()
                 .With(x => x.FlightAvailabilities, new List<FlightAvailability> { flightAvailabilityWithinRange })
                 .Create();
 
@@ -48,20 +50,18 @@ namespace AirportTicketBookingSystem.Tests
         [InlineAutoData("Colombia")]
         [InlineAutoData("Argentina")]
         [InlineAutoData()]
-        public void ShouldGetAFlightIfItsDepartureCountryIs(string departureCountry)
+        public void ShouldReturnFlightWithDepartureCountry(string departureCountry)
         {
             //Arrange
-            var fixture = new Fixture();
-
-            var country = fixture.Build<Country>()
-                .With(x => x.Name, departureCountry )
+            var country = _fixture.Build<Country>()
+                .With(x => x.Name, departureCountry)
                 .Create();
 
-            var airport = fixture.Build<Airport>()
+            var airport = _fixture.Build<Airport>()
                 .With(x => x.Country, country)
                 .Create();
 
-            var flight = fixture.Build<Flight>()
+            var flight = _fixture.Build<Flight>()
                 .With(x => x.DepartureAirport, airport)
                 .Create();
 
@@ -80,20 +80,18 @@ namespace AirportTicketBookingSystem.Tests
         [InlineAutoData("Colombia")]
         [InlineAutoData("Argentina")]
         [InlineAutoData()]
-        public void ShouldGetAFlightIfItsArrivalCountryIs(string arrivalCountry)
+        public void ShouldReturnFlightWithArrivalCountry(string arrivalCountry)
         {
             //Arrange
-            var fixture = new Fixture();
-
-            var country = fixture.Build<Country>()
+            var country = _fixture.Build<Country>()
                 .With(x => x.Name, arrivalCountry)
                 .Create();
 
-            var airport = fixture.Build<Airport>()
+            var airport = _fixture.Build<Airport>()
                 .With(x => x.Country, country)
                 .Create();
 
-            var flight = fixture.Build<Flight>()
+            var flight = _fixture.Build<Flight>()
                 .With(x => x.ArrivalAirport, airport)
                 .Create();
 
@@ -112,16 +110,14 @@ namespace AirportTicketBookingSystem.Tests
         [InlineAutoData("El Dorado")]
         [InlineAutoData("Ezeiza")]
         [InlineAutoData()]
-        public void ShouldGetAFlightIfItsDepartureAirportIs(string departureAirportName)
+        public void ShouldReturnFlightWithDepartureAirport(string departureAirportName)
         {
             //Arrange
-            var fixture = new Fixture();
-
-            var airport = fixture.Build<Airport>()
+            var airport = _fixture.Build<Airport>()
                 .With(x => x.Name, departureAirportName)
                 .Create();
 
-            var flight = fixture.Build<Flight>()
+            var flight = _fixture.Build<Flight>()
                 .With(x => x.DepartureAirport, airport)
                 .Create();
 
@@ -140,16 +136,14 @@ namespace AirportTicketBookingSystem.Tests
         [InlineAutoData("El Dorado")]
         [InlineAutoData("Ezeiza")]
         [InlineAutoData()]
-        public void ShouldGetAFlightIfItsArrivalAirportIs(string arrivalAirportName)
+        public void ShouldReturnFlightWithArrivalAirport(string arrivalAirportName)
         {
             //Arrange
-            var fixture = new Fixture();
-
-            var airport = fixture.Build<Airport>()
+            var airport = _fixture.Build<Airport>()
                 .With(x => x.Name, arrivalAirportName)
                 .Create();
 
-            var flight = fixture.Build<Flight>()
+            var flight = _fixture.Build<Flight>()
                 .With(x => x.ArrivalAirport, airport)
                 .Create();
 
@@ -163,5 +157,54 @@ namespace AirportTicketBookingSystem.Tests
             Assert.Contains(flight, filteredFlights);
             Assert.Contains(arrivalAirportName, filteredFlights[0].ArrivalAirport.Name);
         }
+
+
+        [Theory]
+        [InlineAutoData(2024, 12, 17)]
+        [InlineAutoData(2023, 11, 30)]
+        public void ShouldReturnFlightWithSpecificDepartureDate(int year, int month, int day)
+        {
+            //Arrange
+            DateTime departureDate = new DateTime(year, month, day);
+            var flight = _fixture.Build<Flight>()
+                .With(x => x.DepartureDate, departureDate)
+                .Create();
+
+            var flights = new List<Flight> { flight };
+
+            //Act
+            var filteredFlights = FlightsInventory.SearchFlightsByDate(flights, [year, month, day]);
+
+            //Assert
+            Assert.Single(filteredFlights);
+            Assert.Contains(flight, filteredFlights);
+            Assert.Equal(departureDate, filteredFlights[0].DepartureDate);
+        }
+
+        [Theory]
+        [InlineAutoData()]
+        [InlineAutoData()]
+        public void ShouldReturnFlightWithSpecificFlightClass(FlightClass flightClass)
+        {
+            //Arrange
+            FlightAvailability flightAvailability = _fixture.Build<FlightAvailability>()
+                            .With(fa => fa.FlightClass, flightClass)
+                            .Create();
+
+            var flight = _fixture.Build<Flight>()
+                .With(x => x.FlightAvailabilities, new List<FlightAvailability> { flightAvailability })
+                .Create();
+
+            var flights = new List<Flight> { flight };
+
+            //Act
+            var filteredFlights = FlightsInventory.SearchFlightsByClass(flights, (int)flightClass);
+
+            //Assert
+            Assert.Single(filteredFlights);
+            Assert.Contains(flight, filteredFlights);
+            Assert.Equal(flightClass, filteredFlights[0].FlightAvailabilities[0].FlightClass);
+        }
+
     }
 }
